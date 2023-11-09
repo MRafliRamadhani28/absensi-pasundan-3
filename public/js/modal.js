750ee921570cbae9d859e437a239ec42d5a11887
MRafliRamadhani28 <mrafliramadhani21@gmail.com>
2023-11-09 08:32:29 +0700

    initial commit

---
+++ public/js/modal.js
@@ -0,0 +1,58 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*******************************!*\
+  !*** ./resources/js/modal.js ***!
+  \*******************************/
+
+
+var hide;
+var toggleModal = function toggleModal(modalId) {
+  var show = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : true;
+  var modalEl = document.getElementById(modalId);
+  if (show) {
+    document.body.classList.add('overflow-hidden');
+    var backdropEl = document.createElement('div');
+    backdropEl.setAttribute('mana-modal-backdrop', '');
+    backdropEl.classList.add('modal-backdrop');
+    document.querySelector('body').append(backdropEl);
+    modalEl.classList.add('show');
+    modalEl.setAttribute('aria-modal', 'true');
+    modalEl.setAttribute('role', 'dialog');
+    modalEl.removeAttribute('aria-hidden');
+    hide = function hide(ev) {
+      _handleOutsideClick(ev.target, modalId, modalEl, backdropEl);
+    };
+    modalEl.addEventListener('click', hide, true);
+  } else {
+    modalEl.setAttribute('aria-hidden', 'true');
+    modalEl.removeAttribute('aria-modal');
+    modalEl.removeAttribute('role');
+    modalEl.removeEventListener('click', hide, true);
+    modalEl.classList.remove('show');
+    document.querySelector('[mana-modal-backdrop]').remove();
+    document.body.classList.remove('overflow-hidden');
+  }
+};
+function _handleOutsideClick(target, modalId, modalEl, backdropEl) {
+  if (target === modalEl || target === backdropEl) {
+    toggleModal(modalId, false);
+  }
+}
+window.toggleModal = toggleModal;
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('[data-mana-modal-toggle]').forEach(function (modalToggleEl) {
+    var modalId = modalToggleEl.getAttribute('data-mana-modal-toggle');
+    var modalEl = document.getElementById(modalId);
+    if (modalEl) {
+      if (!modalEl.hasAttribute('aria-hidden') && !modalEl.hasAttribute('aria-modal')) {
+        modalEl.setAttribute('aria-hidden', 'true');
+      }
+      modalToggleEl.addEventListener('click', function () {
+        toggleModal(modalId, modalEl.hasAttribute('aria-hidden', 'true'));
+      });
+    }
+  });
+});
+/******/ })()
+;
